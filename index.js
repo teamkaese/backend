1d03249fc7029852c6c3c10b9a70f32906358ffd
Miguel Caceres <mcaceres@uni-bremen.de>
2019-09-20 22:11:18 +0200

    Fix server

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 /*
 2019 Bremen Germany template for REST Server
 */
-let port = 8080;
 let express = require('express');
+
+const port = process.env.PORT || 8080;
+
 let app = express();
 
 // First get request
 app.get('/', function (req, res) {
   res.send('Server is listening!');
 });
+
+
+let http = require('http');
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
