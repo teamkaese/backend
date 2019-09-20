b272db5bb91047ba57d39ca7240d57846d33ae90
PhlppKnst <philippkynast@gmx.de>
2019-09-20 23:19:36 +0200

    refactoring

--- index.js
+++ index.js
@@ -2,14 +2,11 @@
 2019 Bremen Germany template for REST Server
 */
 let express = require('express');
-const isProduction = process.env.NODE_ENV === 'production';
+let api = require('./routes/api');
 
 let app = express();
 
-var api = require('./routes/api');
-
 const port = process.env.PORT || 8080;
-
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
