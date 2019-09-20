18e81128af7161fb51386436c8b277877e96cf43
Miguel Caceres <mcaceres@uni-bremen.de>
2019-09-21 00:17:18 +0200

    Add Docker-compose

--- index.js
+++ index.js
@@ -2,14 +2,23 @@
 2019 Bremen Germany template for REST Server
 */
 let express = require('express');
-let api = require('./routes/api');
+const mongoose = require('mongoose');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 let app = express();
 
 const port = process.env.PORT || 8080;
 
+app.use('/', api)
+app.use(express.json());
+
+
+
 app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
+  console.log(`Example app listening on port ${port}`);
 });
 
-app.use('/', api)
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET api route. */
 router.get('/api', function(req, res, next) {
   res.send('Server Data');
+  console.log("From Client: " + req.body);
 });
 
 module.exports = router;
